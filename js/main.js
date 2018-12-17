8ace294b531a606632be8a988661c9950e7f1731
hannujarvinen <jarvinen.hannu@gmail.com>
2018-12-18 00:27:05 +0200

    fixed link generation bugs and removed broken weekend links

--- js/main.js
+++ js/main.js
@@ -1,20 +1,26 @@
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 function docInit() {
 	var d = $(bodiv);
-	// js month 0 is January, so here we need to add one to it to make things work
-	for (i = 0; i < 7; i++) {
+	for (i = 0; i < 14; i++) {
 		var now = new Date();
 		now.setDate(now.getDate()-i);
 		dayname = days[now.getDay()];
-		now.setMonth(now.getMonth()+1);
-		cyear = now.getFullYear();
-		month = now.getMonth() + '';
-		day = now.getDate() + '';
-		if (month.length == 1)
-			month = '0' + month;
-		if (day.length == 1)
-			day = '0' + day;
-		current = cyear + '-' + month + '-' + day;
-		d.html(d.html() + '<a href="https://www.hs.fi/lehti/metro/' + current + '"><p>' + current + ' (' + dayname + ')</p></a>');
+		if (dayname != 'Saturday' && dayname != 'Sunday') {
+			cyear = now.getFullYear();
+			month = now.getMonth();
+			// js month 0 is January, so here we need to add one to it to make things work
+			month = month + 1 + '';
+			day = now.getDate() + '';
+			if (month.length == 1)
+				month = '0' + month;
+			if (day.length == 1)
+				day = '0' + day;
+			current = cyear + '-' + month + '-' + day;
+			d.html(d.html() + '<a href="https://www.hs.fi/lehti/metro/' + current + '"><p>' + current + ' (' + dayname + ')</p></a>');
+		}
+		else {
+			if (dayname == 'Sunday')
+				d.html(d.html() + '<i>~~ weekend ~~</i></br>');
+		}
 	}
 };
